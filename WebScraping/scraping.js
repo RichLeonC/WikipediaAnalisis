d7121162ff159c528af8196e9a97d8631e1a026e
RichLeonC <r29leonc@gmail.com>
2022-05-16 10:49:52 -0600

    Update scraping.js

--- WebScraping/scraping.js
+++ WebScraping/scraping.js
@@ -1,1 +1,5 @@
-console.log("Hola")
+
+const cheerio = require("cheerio");// incluir cheerio
+const request = require("request-promise"); // incluir respuestas 
+const fs = require('fs-extra');
+const writeStream =  fs.createWriteStream('wikiviky.csv'); // creacion del archivo
