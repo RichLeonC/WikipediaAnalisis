6b5b6a51aff125f470a448806eeb82d3e22445b8
RichLeonC <r29leonc@gmail.com>
2022-05-16 10:55:05 -0600

    Viki

--- WebScraping/scraping.js
+++ WebScraping/scraping.js
@@ -3,3 +3,13 @@ const cheerio = require("cheerio");// incluir cheerio
 const request = require("request-promise"); // incluir respuestas 
 const fs = require('fs-extra');
 const writeStream =  fs.createWriteStream('wikiviky.csv'); // creacion del archivo
+
+async function inicio(){
+    const $ =  await  request ({// estas lineas de codigo son para trasformar la pagina en un objeto 
+        uri: 'https://en.wikipedia.org/wiki/Web_scraping', // funcion de cheerio para escaneo de pagina web
+        transform :body => cheerio.load(body) //html que se toma de la pagina
+    }) // petición al sitio web que se le queiere hacer web scraping
+    console.log($.html);
+}
+inicio();
+
