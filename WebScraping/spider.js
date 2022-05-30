9cd9ead59c781bbb4c6a1d634e6d19fd42501bb0
IssaAC00 <algueracmp@gmail.com>
2022-05-30 10:28:14 -0600

    Revert "Revert "Revert "Merge branch 'main' of https://github.com/RichLeonC/WikipediaAnalisis"""
    This reverts commit 8adc5452f10ac526ef951d57f581094b55d31766.

--- WebScraping/spider.js
+++ WebScraping/spider.js
@@ -4,19 +4,21 @@ const fs = require('fs-extra');
 
 //Funcion encarga de recorrer todos los links de la pagina pasada por parametro
 module.exports = async function spider(paginaMadre){
-    const $ = await request({
-        uri: paginaMadre, 
-        transform: body => cheerio.load(body) 
-    }) 
+    const $ = await request({// estas lineas de codigo son para trasformar la pagina en un objeto 
+        uri: 'https://en.wikipedia.org/wiki/Special:AllPages?from=a&to=&namespace=0', // funcion de cheerio para escaneo de pagina web
+        transform: body => cheerio.load(body) //html que se toma de la pagina
+    }) // petición al sitio web que se le queiere hacer web scraping
 
     let links = [];
 
-    $('ul[class="mw-allpages-chunk"]').find('a').each((i,el)=>links.push($(el).attr('href'))); //encontramos todos los links que hay en la pagina
+    $('li').find('a').each((i,el)=>links.push($(el).attr('href')));
+
+   // console.log(links);
     return new Promise(function(res,rej){
         res(links);
     })
       
-
+   // https://en.wikipedia.org/wiki/A%22
 }
 
 
