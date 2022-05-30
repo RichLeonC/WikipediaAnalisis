ced23c37302679ea8ae63b0c400e524e59f35571
IssaAC00 <algueracmp@gmail.com>
2022-05-30 14:09:34 -0600

    script

--- Web/DatosBusqueda.sql
+++ Web/DatosBusqueda.sql
@@ -1,2 +1,9 @@
 create database AnalisisDB; 
+use AnalisisDB;
 
+create table Pagina(
+numeroPagina int not null,
+nombrePagina varchar(100) not null,
+palabra varchar(45) not null,
+primary key (numeroPagina, nombrePagina)
+);
