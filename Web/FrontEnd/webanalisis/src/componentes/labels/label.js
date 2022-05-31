fbd67af17257bff6b0c41fe49ba7abe0a133994f
IssaAC00 <algueracmp@gmail.com>
2022-05-30 19:00:04 -0600

    FrontEnd

---
+++ Web/FrontEnd/webanalisis/src/componentes/labels/label.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import './label.css'
+
+export default function label({text}) {
+  return (
+    <div className='label-container'>
+
+            <label> {text}</label>
+
+
+    </div>
+  )
+}
