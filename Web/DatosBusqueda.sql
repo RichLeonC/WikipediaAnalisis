a898c93a93f4c6d08a68a8d283b9e8c5b229dc1f
RichLeonC <r29leonc@gmail.com>
2022-06-02 16:57:13 -0600

    Update

--- Web/DatosBusqueda.sql
+++ Web/DatosBusqueda.sql
@@ -8,4 +8,5 @@ palabra varchar(45) not null,
 primary key (numeroPagina, nombrePagina)
 );
 
+insert into Pagina values(1,'pag1','hello');
 #ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
