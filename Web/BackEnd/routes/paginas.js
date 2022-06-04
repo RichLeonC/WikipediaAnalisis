93a674a08417ca2b31afe3e35a5fd3915310d3f7
RichLeonC <r29leonc@gmail.com>
2022-06-03 18:09:27 -0600

    Update Index.js

--- Web/BackEnd/routes/paginas.js
+++ Web/BackEnd/routes/paginas.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const mySqlConexion = require("../BackEnd/conexionMySQL");
+const mysqlConexion= require("./conexionMySQL");
 
 router.get('/',(req,res)=>{ //req es request
     mysqlConexion.query('select * from Pagina',(error,rows,fields)=>{
@@ -14,13 +14,20 @@ router.get('/',(req,res)=>{ //req es request
 
 router.get('/:palabras',(req,res)=>{ //req es request
     let {palabras} = req.params;
-    
-    mysqlConexion.query('select * from Usuario',(error,rows,fields)=>{
-        if(!error){
-            res.json(rows);
-        }else{
-            console.log(error);
-        }
-    });
+    let arrayJson = [];
+    let arrayPalabras = palabras.split(" ",",");
+
+    arrayPalabras.forEach(ele=>{
+        mysqlConexion.query('select numeroPagina,nombrePagina from Pagina where palabra=?',[ele],(error,rows,fields)=>{
+            if(!error){
+                arrayJson.push(res.json);
+               // res.json(rows);
+            }else{
+                console.log(error);
+            }
+        });
+    })
+
+
 });
 
