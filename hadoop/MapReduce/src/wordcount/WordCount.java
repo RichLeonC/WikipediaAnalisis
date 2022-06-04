ee8df289b2dfd574d2c90c593e0cf9e9961fad92
RonaldCF <ronaldcordero32@gmail.com>
2022-06-03 21:36:50 -0600

    Lectura del excel lista

--- hadoop/MapReduce/src/wordcount/WordCount.java
+++ hadoop/MapReduce/src/wordcount/WordCount.java
@@ -27,33 +27,127 @@ public class WordCount {
         public void map(LongWritable key, Text value, Context context)
                 throws IOException, InterruptedException {
             
-            // transformar el input a string
+            // transformar el input a string            
             String line = value.toString();
             int finTitulos = line.indexOf("|");
             
-            //Titulos        
-            String celdaTitulos = line.substring(0, finTitulos); // captura de solo titulos
-            StringTokenizer titulos = new StringTokenizer(celdaTitulos, ","); // separador de titulos
-                    
-            while(titulos.hasMoreTokens()) {    // conteo de titulos
-                value.set(titulos.nextToken());
-                context.write(value, new IntWritable(1));
-            }//hasta aquí solo agarra titulos
-/*          
-            //Subtitulos
-            int inicioSubtitulos = line.indexOf(finTitulos);
-            String textoSinTitulos = line.substring(inicioSubtitulos);
+//            //Titulos        
+//            String celdaTitulos = line.substring(0, finTitulos); // captura de solo titulos
+//            StringTokenizer titulos = new StringTokenizer(celdaTitulos, ","); // separador de titulos
+//                    
+//            while(titulos.hasMoreTokens()) {    // conteo de titulos
+//                value.set(titulos.nextToken());
+//                context.write(value, new IntWritable(1));
+//            }//hasta aquí solo agarra titulos______________________________________________________________*/
+
+            //Parrafos
+            int finSubtitulos = line.indexOf("|", finTitulos + 1);  // No comentar para que funcionen las demas                   
+ //           String celdaSubtitulos = line.substring(finTitulos, finSubtitulos);
+ //           StringTokenizer subtitulos = new StringTokenizer(celdaSubtitulos, ",");
             
-            int finSubtitulos = textoSinTitulos.indexOf("|");
-            String celdaSubtitulos = textoSinTitulos.substring(0, finSubtitulos);
+ //           while(subtitulos.hasMoreTokens()) {
+ //               value.set(subtitulos.nextToken());
+ //               context.write(value, new IntWritable(1));
+ //           }//Hasta aqui subtitulos_______________________________________________________________________*/
             
-            StringTokenizer subtitulos = new StringTokenizer(celdaSubtitulos, ",");
+            //Columna parrafos
+            int finParrafos = line.indexOf("|", finSubtitulos + 1); // No comentar para que funcionen las demas
+//            String celdaParrafos = line.substring(finSubtitulos, finParrafos);
+//            StringTokenizer parrafos = new StringTokenizer(celdaParrafos, ",");
             
-            while(subtitulos.hasMoreTokens()) { // conteo de subtitulos
-                value.set(subtitulos.nextToken());
+//            while(parrafos.hasMoreTokens()) {
+//                value.set(parrafos.nextToken());
+//                context.write(value, new IntWritable(1));
+//            }
+            
+            //Columna parrafosStemming
+            int finParrafosStemming = line.indexOf("|", finParrafos + 1); // No comentar para que funcionen las demas
+//            String celdaParrafosStemming = line.substring(finParrafos, finParrafosStemming);
+//            StringTokenizer parrafosStemming = new StringTokenizer(celdaParrafosStemming, ",");
+            
+//            while(parrafosStemming.hasMoreTokens()) {
+//                value.set(parrafosStemming.nextToken());
+//                context.write(value, new IntWritable(1));
+//            }
+
+            //Columna TitulosStemming
+            int finTitulosStemming = line.indexOf("|", finParrafosStemming + 1); // No comentar para que funcionen las demas
+//            String celdaTitulosStemming = line.substring(finParrafosStemming, finTitulosStemming);
+//            StringTokenizer titulosStemming = new StringTokenizer(celdaTitulosStemming, ",");
+//            
+//            while(titulosStemming.hasMoreTokens()) {
+//                value.set(titulosStemming.nextToken());
+//                context.write(value, new IntWritable(1));
+//            }
+            
+            //Columna SubtitulosStemming
+            int finSubtitulosStemming = line.indexOf("|", finTitulosStemming + 1); // No comentar para que funcionen las demas
+//            String celdaSubtitulosStemming = line.substring(finTitulosStemming, finSubtitulosStemming);
+//            StringTokenizer subtitulosStemming = new StringTokenizer(celdaSubtitulosStemming, ",");
+            
+//            while(subtitulosStemming.hasMoreTokens()) {
+//                value.set(subtitulosStemming.nextToken());
+//                context.write(value, new IntWritable(1));
+//            }
+
+            //Columna SrcImages
+            int finSrcImages = line.indexOf("|", finSubtitulosStemming + 1); // No comentar para que funcionen las demas
+//            String celdaSrcImages = line.substring(finSubtitulosStemming, finSrcImages);
+//            StringTokenizer srcImages = new StringTokenizer(celdaSrcImages, ",");
+//            
+//            while(srcImages.hasMoreTokens()) {
+//                value.set(srcImages.nextToken());
+//                context.write(value, new IntWritable(1));
+//            }
+
+            //Columna altImages
+            int finAltImages = line.indexOf("|", finSrcImages + 1); // No comentar para que funcionen las demas
+//            String celdaAltImages = line.substring(finSrcImages, finAltImages);
+//            StringTokenizer altImages = new StringTokenizer(celdaAltImages, ",");
+            
+//            while(altImages.hasMoreTokens()) {
+//                value.set(altImages.nextToken());
+//               context.write(value, new IntWritable(1));
+//            }
+
+            //Columna altImagesStemming
+            int finAltImagesStemming = line.indexOf("|", finAltImages + 1); // No comentar para que funcionen las demas
+//            String celdaAltImagesStemming = line.substring(finAltImages, finAltImagesStemming);
+//            StringTokenizer altImagesStemming = new StringTokenizer(celdaAltImagesStemming, ",");
+            
+//            while(altImagesStemming.hasMoreTokens()) {
+//                value.set(altImagesStemming.nextToken());
+//                context.write(value, new IntWritable(1));
+//            }
+
+            //Columna autores
+            int finAutores = line.indexOf("|", finAltImagesStemming + 1); // No comentar para que funcionen las demas
+//            String celdaAutores = line.substring(finAltImagesStemming, finAutores);
+//            StringTokenizer autores = new StringTokenizer(celdaAutores, ",");
+//            
+//            while(autores.hasMoreTokens()) {
+//                value.set(autores.nextToken());
+//                context.write(value, new IntWritable(1));
+//            }
+
+            //Columna referencias
+            int finReferencias = line.indexOf("|", finAutores + 1); // No comentar para que funcionen las demas
+//            String celdaReferencias = line.substring(finAutores, finReferencias);
+//            StringTokenizer referencias = new StringTokenizer(celdaReferencias, ",");
+            
+//            while(referencias.hasMoreTokens()) {
+//                value.set(referencias.nextToken());
+//                context.write(value, new IntWritable(1));
+//            }
+            
+            //Columna Links (aquí no se necesita un index "finLinks porque ya solo debe seguir hasta que se acabe el texto
+            String celdaLinks = line.substring(finReferencias);
+            StringTokenizer links = new StringTokenizer(celdaLinks, ",");
+            
+            while(links.hasMoreTokens()) {
+                value.set(links.nextToken());
                 context.write(value, new IntWritable(1));
             }
-*/
         }
     }
 
